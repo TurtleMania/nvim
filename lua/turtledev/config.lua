8dea940b107b5c64a4568f8ccf9d8f2497d5e105
TurtleMania <turtlebananaman@gmail.com>
2025-06-20 12:10:05 -0400

    Move configuration to another file

---
+++ lua/turtledev/config.lua
@@ -0,0 +1,7 @@
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
--- lua/turtledev/init.lua
+++ lua/turtledev/init.lua
@@ -1,9 +1,2 @@
 require("turtledev.remaps")
-
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
+require("turtledev.config")
