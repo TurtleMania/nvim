ef06e1794191b7d494f3f0d1bcf93e471bc959e9
TurtleMania <turtlebananaman@gmail.com>
2025-06-20 17:32:20 -0400

    Add telescope find buffer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,8 +3,9 @@ return {
     lazy = true,
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files." },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Search files." }
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find file." },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Search files." },
+        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffer." }
     },
     config = function ()
         require("telescope").load_extension("fzy_native")
