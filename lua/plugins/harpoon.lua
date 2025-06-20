631bf002ae8d8b7ad87ef1209636c50e08ec8ec2
TurtleMania <turtlebananaman@gmail.com>
2025-06-20 16:48:13 -0400

    Add harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+    "ThePrimeagen/harpoon", branch = "harpoon2",
+    lazy = true;
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { "<leader>a", function () require("harpoon"):list():add() end, desc = "Add current file to harpoon." },
+        { "<leader>h", function () require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, desc = "Toggle telescope harpoon menu." },
+        { "<C-f>", function () require("harpoon"):list():select(1) end, desc = "Open harpoon file 1." },
+        { "<C-d>", function () require("harpoon"):list():select(2) end, desc = "Open harpoon file 2." },
+        { "<C-s>", function () require("harpoon"):list():select(3) end, desc = "Open harpoon file 3." },
+        { "<C-a>", function () require("harpoon"):list():select(4) end, desc = "Open harpoon file 4." }
+    },
+    config = function ()
+        require("harpoon"):setup()
+    end
+}
